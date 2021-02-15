d1d0f28b8eaf696c63b1f665585ba26e2db579bf
David Oyinbo <dev.davexoyinbo@gmail.com>
2021-02-15 09:24:41 +0100

    Teachers section :Made changes to the page layout

--- pages/teachers/apply.vue
+++ pages/teachers/apply.vue
@@ -1,175 +1,183 @@
 <template>
   <div id="teachers-apply-page">
-    <div class="about-the-learn-room apply-page-section">
-      <h1 class="h1 heading">Step 1 : About The LearnRoom</h1>
-      <div class="highlight">
-        Lorem ipsum dolor sit amet, consectetur adi pisicing elit, sed do
-        eiusmod tempor incididunt ut let dolore magn aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi
-        <a href="#">learn more</a>
-      </div>
-      <generic-button :noBorder="true" :primary="true"
-        >Take the LearnRoom</generic-button
-      >
-    </div>
-    <form action="" class="form">
-      <div class="apply-page-section">
-        <h1 class="h1 heading">Step 2 : Take Your Application</h1>
+    <div class="content">
+      <div class="about-the-learn-room apply-page-section">
+        <h1 class="h1 heading">Step 1 : About The LearnRoom</h1>
         <div class="highlight">
           Lorem ipsum dolor sit amet, consectetur adi pisicing elit, sed do
           eiusmod tempor incididunt ut let dolore magn aliqua. Ut enim ad minim
           veniam, quis nostrud exercitation ullamco laboris nisi
           <a href="#">learn more</a>
         </div>
-
-        <div class="form-group">
-          <label for="">Public Teacher Name</label>
+        <generic-button :noBorder="true" :primary="true"
+          >Take the LearnRoom</generic-button
+        >
+      </div>
+      <form action="" class="form">
+        <div class="apply-page-section">
+          <h1 class="h1 heading">Step 2 : Take Your Application</h1>
           <div class="highlight">
             Lorem ipsum dolor sit amet, consectetur adi pisicing elit, sed do
-            eiusmod tempor
-          </div>
-          <input type="text" class="form-control sm" />
-        </div>
-
-        <div class="form-group">
-          <label for="">Legal Name</label>
-          <div class="highlight">
-            Lorem ipsum dolor sit amet, consectetur adi pisicing elit, sed do
-            eiusmod tempor
-          </div>
-          <input type="text" class="form-control sm" />
-        </div>
-
-        <div class="form-group">
-          <label for="">Email</label>
-          <div class="highlight">
-            Lorem ipsum dolor sit amet, consectetur adi pisicing elit, sed do
-            eiusmod tempor
-          </div>
-          <a href="#">Resend</a>
-        </div>
-
-        <div class="form-group">
-          <label for="">Phone</label>
-          <div class="highlight">
-            Lorem ipsum dolor sit amet, consectetur adi pisicing elit, sed do
-            eiusmod tempor
-          </div>
-          <input type="text" class="form-control sm" />
-        </div>
-
-        <div class="form-group">
-          <div class="highlight">
-            Lorem ipsum dolor sit amet, consectetur adi pisicing eiusmod tempor
-            incididunt ut let empor incididunt ut letdolore magn aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+            eiusmod tempor incididunt ut let dolore magn aliqua. Ut enim ad
+            minim veniam, quis nostrud exercitation ullamco laboris nisi
             <a href="#">learn more</a>
           </div>
-          <textarea type="text" class="form-control"> </textarea>
-        </div>
 
-        <div class="form-group">
-          <div class="highlight">
-            Lorem ipsum dolor sit amet, consectetur adi pisicing eiusmod tempor
-            incididunt ut let empor incididunt ut letdolore magn aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi
-            <a href="#">learn more</a>
-          </div>
-          <textarea type="text" class="form-control"> </textarea>
-        </div>
-
-        <div class="form-group">
-          <div class="highlight">
-            Lorem ipsum dolor sit amet, consectetur adi pisicing eiusmod tempor
-            incididunt ut let empor incididunt ut letdolore magn aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi
-            <a href="#">learn more</a>
-          </div>
-          <textarea type="text" class="form-control"> </textarea>
-        </div>
-      </div>
-
-      <div class="apply-page-section">
-        <h1 class="h1 heading">Step 3 : Tell us about your classes</h1>
-        <div class="form-group">
-          <div class="highlight">
-            Lorem ipsum dolor sit amet, consectetur adi pisicing eiusmod tempor
-            incididunt ut let dolore magn empor incididunt ut letaliqua. Ut enim
-            Lorem ipsum dolor sit amet, consectetur adi pisicing eiusmod tempor
-            incididunt ut let dolore empor incididunt ut let magn aliqua. Ut
-            enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
-            <a href="#">learn more</a>
-          </div>
-          <textarea type="text" class="form-control"> </textarea>
-        </div>
-        <div class="form-group">
-          <div class="highlight">
-            Lorem ipsum dolor sit amet, consectetur adi pisicing eiusmod tempor
-            incididunt ut let dolore magn empor incididunt ut letaliqua. Ut enim
-            Lorem ipsum dolor sit amet, consectetur adi pisicing eiusmod tempor
-            incididunt ut let dolore empor incididunt ut let magn aliqua. Ut
-            enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
-            <a href="#">learn more</a>
-          </div>
-          <textarea type="text" class="form-control"> </textarea>
-        </div>
-      </div>
-
-      <div class="apply-page-section">
-        <h1 class="h1 heading">Step 4 : Teach us something!</h1>
-        <div class="form-group">
-          <div class="highlight">
-            Lorem ipsum dolor sit amet, consectetur adi pisicing eiusmod tempor
-            incididunt ut let dolore magn empor incididunt ut letaliqua. Ut enim
-            Lorem ipsum dolor sit amet, consectetur adi pisicing eiusmod tempor
-            incididunt ut let dolore empor incididunt ut let magn aliqua. Ut
-            enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
-            <a href="#">learn more</a>
-          </div>
-        </div>
-        <ul class="u-list">
-          <li>Lorem ipsum dolor sit amet, consectetur adi pisicing</li>
-          <li>Lorem ipsum dolor sit amet, consectetur adi pisicing</li>
-          <li>Lorem ipsum dolor sit amet, consectetur adi pisicing</li>
-        </ul>
-      </div>
-
-      <div class="record-button-section">
-        <div class="record-button-container">
-          <generic-button :noBorder="true" :primary="true"
-            >Record Your Video</generic-button
-          >
-        </div>
-        <ul class="u-list">
-          <li>Lorem ipsum dolor sit amet, consectetur adi pisicing</li>
-          <li>Lorem ipsum dolor sit amet, consectetur adi pisicing</li>
-          <li>Lorem ipsum dolor sit amet, consectetur adi pisicing</li>
-        </ul>
-      </div>
-
-      <div class="submit-section">
-        <div class="form-group">
-          <div class="tnc-container">
-            <input
-              hidden
-              ref="tncCheckbox"
-              class="tnc-checkbox"
-              type="checkbox"
-              name=""
-              id=""
-            />
-            <div class="checkbox-placeholder" @click="tncCheckboxClicked()">
-              <i class="fas fa-check"></i>
+          <div class="form-group">
+            <label for="">Public Teacher Name</label>
+            <div class="highlight">
+              Lorem ipsum dolor sit amet, consectetur adi pisicing elit, sed do
+              eiusmod tempor
             </div>
-            <span>
-              I agree to the
-              <a href="#"> Terms & Conditions , Community Standards</a>
-            </span>
+            <input type="text" class="form-control sm" />
+          </div>
+
+          <div class="form-group">
+            <label for="">Legal Name</label>
+            <div class="highlight">
+              Lorem ipsum dolor sit amet, consectetur adi pisicing elit, sed do
+              eiusmod tempor
+            </div>
+            <input type="text" class="form-control sm" />
+          </div>
+
+          <div class="form-group">
+            <label for="">Email</label>
+            <div class="highlight">
+              Lorem ipsum dolor sit amet, consectetur adi pisicing elit, sed do
+              eiusmod tempor
+            </div>
+            <a href="#">Resend</a>
+          </div>
+
+          <div class="form-group">
+            <label for="">Phone</label>
+            <div class="highlight">
+              Lorem ipsum dolor sit amet, consectetur adi pisicing elit, sed do
+              eiusmod tempor
+            </div>
+            <input type="text" class="form-control sm" />
+          </div>
+
+          <div class="form-group">
+            <div class="highlight">
+              Lorem ipsum dolor sit amet, consectetur adi pisicing eiusmod
+              tempor incididunt ut let empor incididunt ut letdolore magn
+              aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+              laboris nisi
+              <a href="#">learn more</a>
+            </div>
+            <textarea type="text" class="form-control"> </textarea>
+          </div>
+
+          <div class="form-group">
+            <div class="highlight">
+              Lorem ipsum dolor sit amet, consectetur adi pisicing eiusmod
+              tempor incididunt ut let empor incididunt ut letdolore magn
+              aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+              laboris nisi
+              <a href="#">learn more</a>
+            </div>
+            <textarea type="text" class="form-control"> </textarea>
+          </div>
+
+          <div class="form-group">
+            <div class="highlight">
+              Lorem ipsum dolor sit amet, consectetur adi pisicing eiusmod
+              tempor incididunt ut let empor incididunt ut letdolore magn
+              aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+              laboris nisi
+              <a href="#">learn more</a>
+            </div>
+            <textarea type="text" class="form-control"> </textarea>
           </div>
         </div>
-        <button class="submit-button">Submit Application</button>
-      </div>
-    </form>
+
+        <div class="apply-page-section">
+          <h1 class="h1 heading">Step 3 : Tell us about your classes</h1>
+          <div class="form-group">
+            <div class="highlight">
+              Lorem ipsum dolor sit amet, consectetur adi pisicing eiusmod
+              tempor incididunt ut let dolore magn empor incididunt ut
+              letaliqua. Ut enim Lorem ipsum dolor sit amet, consectetur adi
+              pisicing eiusmod tempor incididunt ut let dolore empor incididunt
+              ut let magn aliqua. Ut enim ad minim veniam, quis nostrud
+              exercitation ullamco laboris nisi
+              <a href="#">learn more</a>
+            </div>
+            <textarea type="text" class="form-control"> </textarea>
+          </div>
+          <div class="form-group">
+            <div class="highlight">
+              Lorem ipsum dolor sit amet, consectetur adi pisicing eiusmod
+              tempor incididunt ut let dolore magn empor incididunt ut
+              letaliqua. Ut enim Lorem ipsum dolor sit amet, consectetur adi
+              pisicing eiusmod tempor incididunt ut let dolore empor incididunt
+              ut let magn aliqua. Ut enim ad minim veniam, quis nostrud
+              exercitation ullamco laboris nisi
+              <a href="#">learn more</a>
+            </div>
+            <textarea type="text" class="form-control"> </textarea>
+          </div>
+        </div>
+
+        <div class="apply-page-section">
+          <h1 class="h1 heading">Step 4 : Teach us something!</h1>
+          <div class="form-group">
+            <div class="highlight">
+              Lorem ipsum dolor sit amet, consectetur adi pisicing eiusmod
+              tempor incididunt ut let dolore magn empor incididunt ut
+              letaliqua. Ut enim Lorem ipsum dolor sit amet, consectetur adi
+              pisicing eiusmod tempor incididunt ut let dolore empor incididunt
+              ut let magn aliqua. Ut enim ad minim veniam, quis nostrud
+              exercitation ullamco laboris nisi
+              <a href="#">learn more</a>
+            </div>
+          </div>
+          <ul class="u-list">
+            <li>Lorem ipsum dolor sit amet, consectetur adi pisicing</li>
+            <li>Lorem ipsum dolor sit amet, consectetur adi pisicing</li>
+            <li>Lorem ipsum dolor sit amet, consectetur adi pisicing</li>
+          </ul>
+        </div>
+
+        <div class="record-button-section">
+          <div class="record-button-container">
+            <generic-button :noBorder="true" :primary="true"
+              >Record Your Video</generic-button
+            >
+          </div>
+          <ul class="u-list">
+            <li>Lorem ipsum dolor sit amet, consectetur adi pisicing</li>
+            <li>Lorem ipsum dolor sit amet, consectetur adi pisicing</li>
+            <li>Lorem ipsum dolor sit amet, consectetur adi pisicing</li>
+          </ul>
+        </div>
+
+        <div class="submit-section">
+          <div class="form-group">
+            <div class="tnc-container">
+              <input
+                hidden
+                ref="tncCheckbox"
+                class="tnc-checkbox"
+                type="checkbox"
+                name=""
+                id=""
+              />
+              <div class="checkbox-placeholder" @click="tncCheckboxClicked()">
+                <i class="fas fa-check"></i>
+              </div>
+              <span>
+                I agree to the
+                <a href="#"> Terms & Conditions , Community Standards</a>
+              </span>
+            </div>
+          </div>
+          <button class="submit-button">Submit Application</button>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -195,6 +203,10 @@ export default {
     color: $primaryColor;
     text-decoration: none;
   }
+
+  .content {
+    max-width: 600px;
+  }
   .apply-page-section {
     margin-bottom: 32px;
   }
@@ -203,7 +215,6 @@ export default {
   }
   .highlight {
     font-size: 0.9rem;
-    max-width: 600px;
   }
 
   .heading + .highlight {
